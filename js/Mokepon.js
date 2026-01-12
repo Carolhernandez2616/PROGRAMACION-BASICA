09f3d924d37670ddc8a48edfbbc6539c81e77ec5
Carolhernandez2616 <hernandezcuervocarolyurley@gmail.com>
2026-01-12 17:57:56 -0500

    Add files via upload

--- js/Mokepon.js
+++ js/Mokepon.js
@@ -9,7 +9,7 @@ function seleccionarMascotaJugador (){
   let inputHipodoge = document.getElementById ('Hipodoge')
   let inputCapipepo = document.getElementById ('Capipepo')
   let inputRatigueya = document.getElementById ('Ratigueya')
-  let spanMascotaJugador = document.getElementById("Mascota-jugador")
+  let spanMascotaJugador = document.getElementById('Mascota-jugador')
   
   
   if(inputHipodoge.checked){
@@ -19,8 +19,35 @@ function seleccionarMascotaJugador (){
   }else if (inputRatigueya.checked){
     spanMascotaJugador.innerHTML = 'Ratigueya'
   }else{
-    alert("Selecciona una mascota")
+    alert('Selecciona una mascota')
   }
+  
+  seleccionarMascotaEnemigo()
+  
 }
+
+function seleccionarMascotaEnemigo(){
+  let ataqueAleatorio = aleatorio(1,3)
+  let spanMascotaEnemigo = document.getElementById("Mascota-enemigo")
+  
+  if (ataqueAleatorio == 1){
+      spanMascotaEnemigo.innerHTML = 'Hipodoge'
+    }else if (ataqueAleatorio == 2){
+      spanMascotaEnemigo.innerHTML = 'Capipepo'
+      
+    }else{
+      spanMascotaEnemigo.innerHTML = 'Ratigueya'
+    }
+      
+    
+}
+
+
+function aleatorio(min, max){
+  return Math.floor(Math.random() *(max -min +1) + min)
+}
+
+
+
 
 window.addEventListener('load' , iniciarJuego)
