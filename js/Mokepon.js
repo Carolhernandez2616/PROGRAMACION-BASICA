4f7bca533eeb38db647be20430fc7753091b719a
Carolhernandez2616 <hernandezcuervocarolyurley@gmail.com>
2026-01-12 16:55:32 -0500

    Add files via upload

--- js/Mokepon.js
+++ js/Mokepon.js
@@ -9,15 +9,15 @@ function seleccionarMascotaJugador (){
   let inputHipodoge = document.getElementById ('Hipodoge')
   let inputCapipepo = document.getElementById ('Capipepo')
   let inputRatigueya = document.getElementById ('Ratigueya')
-  
+  let spanMascotaJugador = document.getElementById("Mascota-jugador")
   
   
   if(inputHipodoge.checked){
-     alert("Seleccionaste a Hipodoge")
+    spanMascotaJugador.innerHTML = 'Hipodoge'
   }else if (inputCapipepo.checked){
-    alert("Seleccionaste a Capipepo")
+    spanMascotaJugador.innerHTML = 'Capipepo'
   }else if (inputRatigueya.checked){
-    alert("Seleccionaste a Ratigueya")
+    spanMascotaJugador.innerHTML = 'Ratigueya'
   }else{
     alert("Selecciona una mascota")
   }
